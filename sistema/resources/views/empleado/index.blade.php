f2c4585229df04c4a2af15cd139c5bebd1c2dd3d
Edison <edison.sanchez@inpsercom.com>
2021-08-18 22:46:05 -0500

    laavel

---
+++ sistema/resources/views/empleado/index.blade.php
@@ -0,0 +1,58 @@
+@extends('layouts.app')
+@section('content')
+<div class="container">
+
+        @if (Session::has('mensaje'))
+            <div class="alert alert-success alert-dismissible" role="alert">
+        {{Session::get('mensaje')}}
+                <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+        @endif
+
+<a href="{{url('empleado/create')}}" class="btn btn-success">Registrar nuevo empleado</a>
+
+<table class="table table-light">
+    <thead class="thead-light">
+        <tr>
+            <th>Foto</th>
+            <th>#</th>
+            <th>Nombre</th>
+            <th>Apellido Materno</th>
+            <th>Apellido Paterno</th>
+            <th>Correo</th>
+            <th>Estado</th>
+            <th>Acciones</th>
+        </tr>
+    </thead>
+    <tbody>
+        @foreach ($empleados as $empleado)
+        <tr>
+            <td>{{$empleado->id}}</td>
+            <td>
+                <img class="img-thumbnail img-fluid" src="{{ asset('storage').'/'.$empleado->Foto }}" width="100" alt="">
+            </td>
+            <td>{{$empleado->Nombre}}</td>
+            <td>{{$empleado->ApellidoPaterno}}</td>
+            <td>{{$empleado->ApellidoMaterno}}</td>
+            <td>{{$empleado->Correo}}</td>
+            <td>{{$empleado->Estado}}</td>
+            <td>
+                <a href="{{url('/empleado/'.$empleado->id.'/edit')}}" class="btn btn-warning">
+                    Editar
+                </a>
+                |
+                <form action="{{url('/empleado/'.$empleado->id)}}" method="post" class="d-inline">
+                 @csrf
+                 {{method_field('DELETE')}}
+                    <input type="submit" onclick="return confirm('¿Quieres borrar?')" value="Borrar" class="btn btn-danger">
+                </form>
+            </td>
+        </tr>
+        @endforeach
+    </tbody>
+</table>
+{!!$empleados->links() !!}
+</div>
+@endsection
